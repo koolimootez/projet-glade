14a3a399171842f6d95f2f250a395346e765a232
mootez <mootez.kooli@esprit.tn>
2022-11-17 01:33:36 +0100

    first commit

---
+++ src/callbacks.c
@@ -0,0 +1,51 @@
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#include <gtk/gtk.h>
+
+#include "callbacks.h"
+#include "interface.h"
+#include "support.h"
+
+
+void
+on_Login_button_clicked                (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_sincrire_button_clicked             (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_reinitialiser_mot_de_passe_button_clicked
+                                        (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_sauvegarder_button_clicked          (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_se_connecter_button_clicked         (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
---
+++ src/callbacks.h
@@ -0,0 +1,23 @@
+#include <gtk/gtk.h>
+
+
+void
+on_Login_button_clicked                (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_sincrire_button_clicked             (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_reinitialiser_mot_de_passe_button_clicked
+                                        (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_sauvegarder_button_clicked          (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_se_connecter_button_clicked         (GtkButton       *button,
+                                        gpointer         user_data);
